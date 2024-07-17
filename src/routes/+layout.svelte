2ac86e7124e0e5ee61b31c6b4212f5a2ebc37a62
Virya Dharmawan <viryadharmawan@gmail.com>
2024-07-17 21:45:04 +0700

    Commit missing layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script>
-    import "../app.css";
+  import "../app.css";
+  import { pwaInfo } from 'virtual:pwa-info'; 
+  import { pwaAssetsHead } from 'virtual:pwa-assets/head';
+
+  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 </script>
+
+<svelte:head> 
+  {#if pwaAssetsHead.themeColor}
+	  <meta name="theme-color" content={pwaAssetsHead.themeColor.content} />
+	{/if}
+	{#each pwaAssetsHead.links as link}
+	  <link {...link} />
+	{/each}
+ 	{@html webManifestLink} 
+</svelte:head>
 
 <nav class="navbar bg-base-100 py-4">
   <div class="flex-1">
