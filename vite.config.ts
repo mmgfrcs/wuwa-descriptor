4c558a1246316e93ae12d427bf499cd60769af45
Virya Dharmawan <viryadharmawan@gmail.com>
2024-07-17 19:48:49 +0700

    Fix manifest and PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,40 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit(), SvelteKitPWA()],
+	plugins: [enhancedImages(), sveltekit(), SvelteKitPWA({
+		useCredentials: true,
+		manifest: {
+			name: "Wuthering Waves Descriptor",
+			short_name: "WuWa Descriptor",
+			icons: [
+					{
+							src: "/android-chrome-192x192.png",
+							sizes: "192x192",
+							type: "image/png"
+					},
+					{
+							src: "/android-chrome-256x256.png",
+							sizes: "256x256",
+							type: "image/png"
+					},
+					{
+							src: "/android-chrome-256x256.png",
+							sizes: "256x256",
+							type: "image/png",
+							purpose: "any"
+					},
+					{
+							src: "/android-chrome-256x256.png",
+							sizes: "256x256",
+							type: "image/png",
+							purpose: "maskable"
+					}
+			],
+			theme_color: "#222222",
+			background_color: "#222222",
+			display: "standalone"
+		}
+	})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
