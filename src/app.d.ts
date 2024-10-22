f39ec11c614b43da1f7dc932b25b4843d9c433b8
Virya Dharmawan <viryadharmawan@gmail.com>
2024-10-23 02:53:55 +0700

    Hidden About Page

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,6 +10,14 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+
+	declare module '*.md' {
+		import type { SvelteComponent } from 'svelte'
+	
+		export default class Comp extends SvelteComponent{}
+	
+		export const metadata: Record<string, unknown>
+	}
 }
 
 export {};
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import About from './about.md'
+</script>
+
+<svelte:head>
+    <title>About Wuwa Descriptor</title>
+</svelte:head>
+
+<div class="prose mx-auto">
+    <About />
+</div>
